57ebeda8d31e7040c0c980a6027df9db094d943e
forrestbenton <elliottbenton@gmail.com>
2018-07-27 23:43:06 -0400

    completed pigeon organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  dupe_array = []
+  return_hash = {}
+  data.each do |key1,value1|
+    value1.each do |key2,value2|
+      value2.each do |name|
+        dupe_array << name
+      end
+    end
+  end
+  operating_array = dupe_array.uniq
+
+  operating_array.each do |name|
+    return_hash[name] = {:color => [], :gender => [], :lives => []}
+    data.each do |key1,value1|
+      value1.each do |key2,value2|
+        i = 0
+        while i < value2.length
+          if value2[i] == name
+            return_hash[name][key1] << key2.to_s
+          end
+          i += 1
+        end
+      end
+    end
+  end
+    return_hash
+
 end
